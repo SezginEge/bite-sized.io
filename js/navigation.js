07e91534aeb9b376f31f3a330078d79004523c64
SezginEge <SezginEge@users.noreply.github.com>
2021-12-05 20:51:13 +0000

    deploy: b2bf8b327b61d1cb7f0a990821a045c89fc18c19

---
+++ js/navigation.js
@@ -0,0 +1,12 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    const btn = document.querySelector("button.mobile-menu-button");
+    const menu = document.querySelector(".mobile-menu");
+
+    btn.addEventListener("click", () => {
+      menu.classList.toggle("hidden");
+    });
+  },
+  false
+);
